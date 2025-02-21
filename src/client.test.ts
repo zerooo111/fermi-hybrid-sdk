6b600339c661e77aae15c6596693d49b4f2282e7
zerooo111 <zero@fermilabs.xyz>
2025-02-21 08:37:19 +0530

    comment out test file

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,8 +1,8 @@
-import { SDKClient } from './client';
+// import { SDKClient } from './client';
 
-describe('SDKClient', () => {
-  it('should initialize with config', () => {
-    const client = new SDKClient({ apiKey: 'test-key' });
-    expect(client).toBeInstanceOf(SDKClient);
-  });
-}); 
+// describe('SDKClient', () => {
+//   it('should initialize with config', () => {
+//     const client = new SDKClient({ apiKey: 'test-key' });
+//     expect(client).toBeInstanceOf(SDKClient);
+//   });
+// }); 
